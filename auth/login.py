6189bddce0dceb520ab0ace01a28c88a17dbcc93
GeonHae Lee <yoandyo2000@gmail.com>
2024-11-11 18:13:22 +0900

    Add files via upload

--- auth/login.py
+++ auth/login.py
@@ -1,16 +1,8 @@
-# auth/authentication.py
+# auth/login.py
 
-from data.database import get_user, add_user
+from data.database import login_user
 
-def login(username):
-    """사용자를 로그인 처리합니다. 존재하지 않으면 새로 추가합니다."""
-    user = get_user(username)
-    if not user:
-        # 사용자가 존재하지 않으면 새로 추가
-        success = add_user(username)
-        if success:
-            return True, "새로운 사용자로 로그인되었습니다."
-        else:
-            return False, "사용자 추가에 실패했습니다."
-    return True, "로그인 성공!"
-
+def login(username, password):
+    """사용자 인증을 수행합니다."""
+    success, message, user_id = login_user(username, password)
+    return success, message, user_id
